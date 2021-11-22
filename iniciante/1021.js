5148721cd7a7e0c5708828542ed7a99df7b9c87d
levxyca <leticiacaroline-123@hotmail.com>
2021-11-22 10:14:11 -0300

    :memo: exercise 1021 js

--- iniciante/1021.js
+++ iniciante/1021.js
@@ -7,24 +7,29 @@ let aux;
 let notas = [100, 50, 20, 10, 5, 2];
 let qtdNotas = new Array(6);
 
-let moedas = [1, 0.5, 0.25, 0.1, 0.05, 0.01];
+let moedas = [1.0, 0.5, 0.25, 0.1, 0.05, 0.01];
 let qtdMoedas = new Array(7);
 
-n = parseFloat(n).toFixed(2);
+n = parseFloat(n);
+
 aux = n;
 
 console.log("NOTAS:");
 
 for (let i = 0; i <= 5; i++) {
-  qtdNotas[i] = parseFloat(aux / notas[i]);
+  qtdNotas[i] = parseInt(aux / notas[i]);
   aux = aux - qtdNotas[i] * notas[i];
 
   console.log(`${qtdNotas[i]} nota(s) de R$ ${notas[i]},00`);
 }
 
+console.log("MOEDAS:");
+
+aux = aux.toFixed(2);
+
 for (let i = 0; i <= 6; i++) {
-    qtdMoedas[i] = parseFloat(aux / moedas[i]);
-    aux = aux - qtdMoedas[i] * moedas[i];
-  
-    console.log(`${qtdMoedas[i]} nota(s) de R$ ${moedas[i]},00`);
-  }
+  qtdMoedas[i] = (aux / moedas[i]);
+  aux = aux - qtdMoedas[i] * moedas[i];
+
+  console.log(`${qtdMoedas[i]} nota(s) de R$ ${moedas[i]}`);
+}
