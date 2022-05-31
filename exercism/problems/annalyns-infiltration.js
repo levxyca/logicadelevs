8939194c60a3eb590b1b8fd7a67227e0f1d83d6f
levxyca <leticiacaroline-123@hotmail.com>
2022-05-31 13:08:22 -0300

    :sparkles: annalyns infiltration

---
+++ exercism/problems/annalyns-infiltration.js
@@ -0,0 +1,68 @@
+// @ts-check
+
+/**
+ * The fast attack is available when the knight is sleeping
+ *
+ * @param {boolean} knightIsAwake
+ *
+ * @return {boolean} Whether or not you can execute a fast attack.
+ */
+
+export function canExecuteFastAttack(knightIsAwake) {
+  /**
+   * Operador Condicional Ternário
+   *
+   * O operador condicional (ternário) é o único operador JavaScript que possui três operandos. Este operador é frequentemente
+   * usado como um atalho para a instrução if.
+   *
+   * condition ? expr1 : expr2
+   */
+  return !knightIsAwake;
+}
+
+/**
+ * A useful spy captures information, which they can't do if everyone's asleep.
+ *
+ * @param {boolean} knightIsAwake
+ * @param {boolean} archerIsAwake
+ * @param {boolean} prisonerIsAwake
+ *
+ * @returns {boolean} Whether or not you can spy on someone.
+ */
+export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
+  return knightIsAwake || archerIsAwake || prisonerIsAwake;
+}
+
+/**
+ * You'll get caught by the archer if you signal while they're awake.
+ *
+ * @param {boolean} archerIsAwake
+ * @param {boolean} prisonerIsAwake
+ *
+ * @returns {boolean} Whether or not you can send a signal to the prisoner.
+ */
+export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
+  return !archerIsAwake && prisonerIsAwake;
+}
+
+/**
+ * The final stage in the plan: freeing Annalyn's best friend.
+ *
+ * @param {boolean} knightIsAwake
+ * @param {boolean} archerIsAwake
+ * @param {boolean} prisonerIsAwake
+ * @param {boolean} petDogIsPresent
+ *
+ * @returns {boolean} Whether or not you can free Annalyn's friend.
+ */
+export function canFreePrisoner(
+  knightIsAwake,
+  archerIsAwake,
+  prisonerIsAwake,
+  petDogIsPresent
+) {
+  return (
+    (!archerIsAwake && petDogIsPresent) ||
+    (prisonerIsAwake && !knightIsAwake && !archerIsAwake)
+  );
+}
